1e51624e88c47b5654fcea5a23972f5cf886a3ea
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-04-15 22:00:28 +0300

    Initial commit for Props Basics Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
+// src/NavBar.js
 import React from "react";
 
 function NavBar() {
   return (
     <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
+      <h1>Portfolio</h1>
     </nav>
   );
 }
